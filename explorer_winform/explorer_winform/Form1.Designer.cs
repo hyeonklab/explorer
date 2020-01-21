53c50dcda01d8a33713d63e2dae65f1e951ce2cd
hyeonklab <hyeonklab@gmail.com>
2020-01-21 22:19:18 +0900

    first commit

---
+++ explorer_winform/explorer_winform/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace explorer_winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboListViewMode = new System.Windows.Forms.ComboBox();
+            this.txtCurPath = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.cboListViewMode);
+            this.splitContainer1.Panel2.Controls.Add(this.txtCurPath);
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(697, 410);
+            this.splitContainer1.SplitterDistance = 232;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(3, 24);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(227, 386);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.BeforeCollapse += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_beforeCollapse);
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_beforeExpand);
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_nodeClick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "hard_drive_windows_2.ico");
+            this.imageList1.Images.SetKeyName(1, "hard_drive.ico");
+            this.imageList1.Images.SetKeyName(2, "closed_folder.ico");
+            this.imageList1.Images.SetKeyName(3, "opened_folder.ico");
+            this.imageList1.Images.SetKeyName(4, "Hopstarter-Sleek-Xp-Basic-Document-Blank.ico");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // cboListViewMode
+            // 
+            this.cboListViewMode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboListViewMode.FormattingEnabled = true;
+            this.cboListViewMode.Location = new System.Drawing.Point(337, 2);
+            this.cboListViewMode.Name = "cboListViewMode";
+            this.cboListViewMode.Size = new System.Drawing.Size(121, 20);
+            this.cboListViewMode.TabIndex = 2;
+            this.cboListViewMode.SelectedIndexChanged += new System.EventHandler(this.cboListViewMode_selectedIndexChanged);
+            // 
+            // txtCurPath
+            // 
+            this.txtCurPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCurPath.Location = new System.Drawing.Point(2, 2);
+            this.txtCurPath.Name = "txtCurPath";
+            this.txtCurPath.Size = new System.Drawing.Size(334, 21);
+            this.txtCurPath.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.LargeImageList = this.imageList2;
+            this.listView1.Location = new System.Drawing.Point(0, 24);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(461, 386);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "hard_drive_windows_2.ico");
+            this.imageList2.Images.SetKeyName(1, "hard_drive.ico");
+            this.imageList2.Images.SetKeyName(2, "closed_folder.ico");
+            this.imageList2.Images.SetKeyName(3, "opened_folder.ico");
+            this.imageList2.Images.SetKeyName(4, "Hopstarter-Sleek-Xp-Basic-Document-Blank.ico");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 410);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "explorer";
+            this.Load += new System.EventHandler(this.form1_load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ComboBox cboListViewMode;
+        private System.Windows.Forms.TextBox txtCurPath;
+        private System.Windows.Forms.ImageList imageList2;
+    }
+}
+
---
+++ explorer_winform/explorer_winform/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace explorer_winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //보기 옵션 콤보 설정
+            cboListViewMode.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboListViewMode.Items.Add("Details");
+            cboListViewMode.Items.Add("SmallIcon");
+            cboListViewMode.Items.Add("LargeIcon");
+            cboListViewMode.Items.Add("List");
+            cboListViewMode.Items.Add("Tile");
+            cboListViewMode.SelectedIndex = 0;
+        }
+
+        private void form1_load(object sender, EventArgs e)
+        {
+            // 현재 사용자 정보 표시
+            System.Security.Principal.WindowsIdentity identity = System.Security.Principal.WindowsIdentity.GetCurrent();
+            label1.Text = "현재 사용자: " + identity.Name;
+
+            // 현재 로컬 컴퓨터에 존재하는 드라이브 정보 검색, 트리노드에 추가
+            DriveInfo[] all_drives = DriveInfo.GetDrives();
+            foreach( DriveInfo dname in all_drives )
+            {
+                if( dname.DriveType == DriveType.Fixed )
+                {
+                    if( dname.Name == @"C:\" )
+                    {
+                        TreeNode root_node = new TreeNode(dname.Name);
+                        root_node.ImageIndex = 0;
+                        root_node.SelectedImageIndex = 0;
+                        treeView1.Nodes.Add(root_node);
+                        Fill(root_node);
+                    }
+                    else
+                    {
+                        TreeNode root_node = new TreeNode(dname.Name);
+                        root_node.ImageIndex = 1;
+                        root_node.SelectedImageIndex = 1;
+                        treeView1.Nodes.Add(root_node);
+                        Fill(root_node);
+                    }
+                }
+            }
+
+            // 첫번째 노드 확장
+            treeView1.Nodes[0].Expand();
+
+            // list view 보기 속성 설정
+            //listView1.View = View.Details;
+
+            // list view detail 속성 위한 헤더 추가
+            listView1.Columns.Add("이름", listView1.Width / 4, HorizontalAlignment.Left);
+            listView1.Columns.Add("수정한 날짜", listView1.Width / 4, HorizontalAlignment.Left);
+            listView1.Columns.Add("유형", listView1.Width / 4, HorizontalAlignment.Left);
+            listView1.Columns.Add("크기", listView1.Width / 4, HorizontalAlignment.Left);
+
+            // 행 단위 선택 가능
+            listView1.FullRowSelect = true;
+        }
+
+        private void Fill(TreeNode dirNode)
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(dirNode.FullPath);
+                // 드라이브의 하위 폴더 추가
+                foreach( DirectoryInfo dirItem in dir.GetDirectories() )
+                {
+                    TreeNode newNode = new TreeNode(dirItem.Name);
+                    newNode.ImageIndex = 2;
+                    newNode.SelectedImageIndex = 2;
+                    dirNode.Nodes.Add(newNode);
+                    newNode.Nodes.Add("*");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("catch error: " + ex.Message);
+            }
+        }
+
+        // 트리 확장되기 전 발생하는 이벤트
+        private void treeView1_beforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            //if( e.Node.Nodes[0].Text == "*" || )
+            if ( e.Node.ImageIndex == 2  )  // closed icon이면, opened icon으로 변경
+            {
+                e.Node.Nodes.Clear();
+                e.Node.ImageIndex = 3;
+                e.Node.SelectedImageIndex = 3;
+                Fill(e.Node);
+            }
+        }
+
+        // 트리 닫히기 전 발생 이벤트
+        private void treeView1_beforeCollapse(object sender, TreeViewCancelEventArgs e)
+        {
+            //if( e.Node.Nodes[0].Text == "*" )
+            if ( e.Node.ImageIndex == 3  )  // opened icon이면, closed icon 으로 변경
+            {
+                e.Node.ImageIndex = 2;
+                e.Node.SelectedImageIndex = 2;
+            }
+        }
+
+        // 트리 클릭시 발생 이벤트
+        private void treeView1_nodeClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            settingListView(e.Node.FullPath);
+        }
+
+        // 우측 list view 그리기
+        private void settingListView(string fullPath)
+        {
+            try
+            {
+                // 기존 파일 목록 제거
+                listView1.Items.Clear();
+                // 현재 경로 표시
+                txtCurPath.Text = fullPath;
+                DirectoryInfo dir = new DirectoryInfo(fullPath);
+
+                int directCount = 0;
+                // 하위 디렉토리 보여주기
+                foreach( DirectoryInfo dirItem in dir.GetDirectories() )
+                {
+                    // 하위 디렉토리 존재하면, list view에 추가
+                    // list view item 객체 생성
+                    ListViewItem lvItem = new ListViewItem();
+
+                    // 생성된 list view item 객체에 똑같은 이미지 할당
+                    //dirItem.Attributes.GetType()
+                    // todo icon 이미지 동적 할당
+                    lvItem.ImageIndex = 2;
+                    lvItem.Text = dirItem.Name;
+
+                    // 아이템을 list view에 추가
+                    listView1.Items.Add(lvItem);
+
+                    listView1.Items[directCount].SubItems.Add(dirItem.CreationTime.ToString());
+                    listView1.Items[directCount].SubItems.Add("폴더");
+                    listView1.Items[directCount].SubItems.Add(dirItem.GetFiles().Length.ToString() + " files");
+                    directCount++;
+                }
+
+                // 디렉토리에 존재하는 파일목록 보여주기
+                FileInfo[] files = dir.GetFiles();
+                int count = 0;
+                foreach( FileInfo fileinfo in files )
+                {
+                    ListViewItem lvItem = new ListViewItem();
+                    lvItem.ImageIndex = 4;
+                    lvItem.Text = fileinfo.Name;
+                    listView1.Items.Add(lvItem);
+                    //listView1.Items.Add(fileinfo.Name);
+
+                    if (fileinfo.LastWriteTime != null)
+                    {
+                        listView1.Items[count].SubItems.Add(fileinfo.LastWriteTime.ToString());
+                    }
+                    else
+                    {
+                        listView1.Items[count].SubItems.Add(fileinfo.CreationTime.ToString());
+                    }
+
+                    listView1.Items[count].SubItems.Add(fileinfo.Attributes.ToString());
+                    listView1.Items[count].SubItems.Add(fileinfo.Length.ToString());
+                    count++;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("catch error: " + ex.Message);
+            }
+        }
+
+        // list view mode 변경 처리
+        private void cboListViewMode_selectedIndexChanged(object sender, EventArgs e)
+        {
+            switch( cboListViewMode.Text)
+            {
+                case "Details":
+                    listView1.View = View.Details;
+                    break;
+                case "SmallIcon":
+                    listView1.View = View.SmallIcon;
+                    break;
+                case "LargeIcon":
+                    listView1.View = View.LargeIcon;
+                    break;
+                case "List":
+                    listView1.View = View.List;
+                    break;
+                case "Tile":
+                    listView1.View = View.Tile;
+                    break;
+            }
+        }
+
+
+
+    }
+}
